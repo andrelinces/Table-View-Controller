037931b2fe798ad3216ad87ffadcf2264676491e
Andre Linces <andrelinces@yahoo.com.br>
2021-08-25 21:12:49 -0300

    necessário criar 3 métodos para utilizar a table view.

--- Table View Controller/ViewController.swift
+++ Table View Controller/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController{
+    
+    var dados: [String] = ["Lasanha", "Pizza", "Tropeiro"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dados.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //var _: IndexPath.Index
+        let celulaReuso = "celulaReuso"
+        let celula = tableView.dequeueReusableCell(withIdentifier: celulaReuso, for: indexPath)
+        celula.textLabel?.text = dados [indexPath.row]
+        return celula
+        
+    }
 }
 
